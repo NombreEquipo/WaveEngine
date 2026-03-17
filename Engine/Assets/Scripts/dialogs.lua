d77d570e82befaa3325c6098d1eb4e920f921ba3
Aniol Lopez Ortega <99729558+Aniolobolo@users.noreply.github.com>
2026-03-17 22:00:30 +0100

    LastFixForReal
    ⚓

--- Engine/Assets/Scripts/dialogs.lua
+++ Engine/Assets/Scripts/dialogs.lua
@@ -4,12 +4,12 @@ return {
         dialogs = {
             {
                 character = "Atenea",
-                portrait  = "Textures/Portrait_Placeholder.png",
+                portrait  = "Textures/Atenea.png",
                 text      = "Telémaco, finalmente te encuentras en la isla. Para llegar al inframundo deberás adentrarte en el bósque de Perséfone, pero te advierto que su aspecto pacífico esconde un mal del que no te puedes descuidar."
             },
             {
                 character = "Telemaco",
-                portrait  = "Textures/Portrait_Placeholder3.png",
+                portrait  = "Textures/Telemaco.png",
                 text      = "De acuerdo. Padre, al fin podré traerte de vuelta a Ítaca."
             }
         }
@@ -29,12 +29,12 @@ return {
         dialogs = {
             {
                 character = "Atenea",
-                portrait  = "Textures/Portrait_Placeholder.png",
+                portrait  = "Textures/Atenea.png",
                 text      = "Telémaco, has encontrado uno de mis altares. Si los activas te mantendrán seguro. Estate atento, los podrás hallar cuando más los necesites."
             },
             {
                 character = "Telemaco",
-                portrait  = "Textures/Portrait_Placeholder3.png",
+                portrait  = "Textures/Telemaco.png",
                 text      = "¡Genial! Lo tendré en cuenta. Me siento mucho más seguro sabiendo que estarás siempre ahí, de alguna manera. Atenea, te lo agradezco."
             }
         }
@@ -44,12 +44,12 @@ return {
         dialogs = {
             {
                 character = "Atenea",
-                portrait  = "Textures/Portrait_Placeholder.png",
+                portrait  = "Textures/Atenea.png",
                 text      = "Ante tus ojos se exhibe el antiguo santuario divino de la isla. Utilizado como lugar de culto hacia Hades, convirtiendolo así en un puente hacia el inframundo. Deberás reactivar el ritual para poder salvar a tu padre. Si exploras la isla podrías encontrar la respuesta que necesitas para hacerlo."
             },
             {
                 character = "Telemaco",
-                portrait  = "Textures/Portrait_Placeholder3.png",
+                portrait  = "Textures/Telemaco.png",
                 text      = "Parece complejo… pero no me rendiré ahora, lograré abrir la puerta al inframundo y rescatar a padre."
             }
         }
