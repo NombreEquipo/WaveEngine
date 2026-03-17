ebd9a252e116f5aeb78c937c7a02cfadf82ac682
Aniol Lopez Ortega <99729558+Aniolobolo@users.noreply.github.com>
2026-03-17 19:41:21 +0100

    dialogs implemented
    🐜

--- Engine/Assets/Scripts/dialogs.lua
+++ Engine/Assets/Scripts/dialogs.lua
@@ -3,29 +3,54 @@ return {
         id = "intro",
         dialogs = {
             {
-                character = "Telemaco",
-                portrait  = "Textures/Portrait_Placeholder3.png",
-                text      = "¿De quien es la voz que oigo ? ."
-            },
-            {
                 character = "Atenea",
                 portrait  = "Textures/Portrait_Placeholder.png",
-                text      = "Saludos Telemaco de Ítaca, soy Atenea, Diosa de la sabíduria, dejame ayudarte en tu camino ."
+                text      = "Telémaco, finalmente te encuentras en la isla. Para llegar al inframundo deberás adentrarte en el bósque de Perséfone, pero te advierto que su aspecto pacífico esconde un mal del que no te puedes descuidar."
             },
             {
                 character = "Telemaco",
                 portrait  = "Textures/Portrait_Placeholder3.png",
-                text      = "Oh, la gran Atenea de las histórias de padre, agradezco su ayuda oh gran deidad."
+                text      = "De acuerdo. Padre, al fin podré traerte de vuelta a Ítaca."
             }
         }
     },
-    tutorial_movement = {
-        id = "tutorial_movement",
+    praise = {
+        id = "praise",
         dialogs = {
             {
-                character = "John cartel",
-                portrait  = "Textures/Portrait_Placeholder2.png",
-                text      = "Hey, soy John cartel, aqui hay una história muy épica e interesante pero no te dejo leerla porque no me apetece compartirla contigo."
+                character = "Atenea",
+                portrait  = "Textures/Portrait_Placeholder.png",
+                text      = "Te mueves fenomenal, aprovecha tus habilidades durante la travesía."
+            }
+        }
+    },
+    checkpointInfo = {
+        id = "checkpointInfo",
+        dialogs = {
+            {
+                character = "Atenea",
+                portrait  = "Textures/Portrait_Placeholder.png",
+                text      = "Telémaco, has encontrado uno de mis altares. Si los activas te mantendrán seguro. Estate atento, los podrás hallar cuando más los necesites."
+            },
+            {
+                character = "Telemaco",
+                portrait  = "Textures/Portrait_Placeholder3.png",
+                text      = "¡Genial! Lo tendré en cuenta. Me siento mucho más seguro sabiendo que estarás siempre ahí, de alguna manera. Atenea, te lo agradezco."
+            }
+        }
+    },
+    sanctuaryInfo = {
+        id = "sanctuaryInfo",
+        dialogs = {
+            {
+                character = "Atenea",
+                portrait  = "Textures/Portrait_Placeholder.png",
+                text      = "Ante tus ojos se exhibe el antiguo santuario divino de la isla. Utilizado como lugar de culto hacia Hades, convirtiendolo así en un puente hacia el inframundo. Deberás reactivar el ritual para poder salvar a tu padre. Si exploras la isla podrías encontrar la respuesta que necesitas para hacerlo."
+            },
+            {
+                character = "Telemaco",
+                portrait  = "Textures/Portrait_Placeholder3.png",
+                text      = "Parece complejo… pero no me rendiré ahora, lograré abrir la puerta al inframundo y rescatar a padre."
             }
         }
     }
