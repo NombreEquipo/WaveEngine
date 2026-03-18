10c2fd2af5ce8eaacc6f1a88bad5fadde4a468bf
PauGutsens <159762692+PauGutsens@users.noreply.github.com>
2026-03-18 09:48:34 +0100

    Cambio de escena

--- Engine/Assets/Scripts/SceneLoader_lvl2.lua
+++ Engine/Assets/Scripts/SceneLoader_lvl2.lua
@@ -1,63 +1,57 @@
---- SceneLoader.lua
+--- SceneLoader_lvl2.lua
+
 
 public = {
-    temp1 = { type = "Scene", value = "" },
-    temp2 = { type = "Scene", value = "" }
+    targetScene = { type = "Scene", value = "" },
+    musicState  = "Level1"   
 }
 
-local scenesPath = Engine.GetScenesPath()
-local musicSource
-local isPlaying = false
-local playerInside = false
+
 
 function Start(self)
-	self.gameObject:SetPersistency(1)
-	musicSource = self.gameObject:GetComponent("Audio Source")
-	Audio.SetMusicState("MainMenu")
+    self.playerInside = false
+    Engine.Log("[SceneLoader_lvl2] Script iniciado en: " .. self.gameObject:GetName())
 
-	if not isPlaying then
-		musicSource:PlayAudioEvent()
-		isPlaying = true
-	end
+    if self.public.targetScene == "" then
+        Engine.Log("[SceneLoader_lvl2] AVISO: 'targetScene' esta vacio. Asignalo en el Inspector.")
+    else
+        Engine.Log("[SceneLoader_lvl2] Escena destino: " .. tostring(self.public.targetScene))
+    end
 end
 
 
 function Update(self, dt)
+    if self.playerInside and Input.GetKeyDown("Space") then
+        local sceneName = self.public.targetScene
+        if sceneName == nil or sceneName == "" then
+            Engine.Log("[SceneLoader_lvl2] ERROR: No hay escena asignada en 'targetScene'.")
+            return
+        end
 
-	-- Teclas de debug para cambio de escena manual
-    if Input.GetKeyDown("1") then
-		Audio.SetMusicState("MainMenu")
-        Engine.LoadScene(scenesPath, self.public.temp1)
+        Engine.Log("[SceneLoader_lvl2] Cargando escena: " .. sceneName)
+
+        -- Cambiar estado de musica si se ha configurado
+        if self.public.musicState and self.public.musicState ~= "" then
+            Audio.SetMusicState(self.public.musicState)
+        end
+
+        local scenesPath = Engine.GetScenesPath()
+        Engine.LoadScene(scenesPath, sceneName)
     end
-
-    if Input.GetKeyDown("2") then
-		Audio.SetMusicState("Level1")
-        Engine.LoadScene(scenesPath, self.public.temp2)
-    end
-
-	-- Cambio de escena por trigger + espacio
-	if playerInside and Input.GetKeyDown("Space") then
-		Audio.SetMusicState("Level1")
-		Engine.LoadScene(scenesPath, self.public.temp1)
-	end
-
 end
 
 
 function OnTriggerEnter(self, other)
-	if other:CompareTag("Player") then
-		playerInside = true
-	end
+    if other:CompareTag("Player") then
+        self.playerInside = true
+        Engine.Log("[SceneLoader_lvl2] Jugador entro en el trigger. Pulsa Espacio para cambiar de escena.")
+    end
 end
 
 
 function OnTriggerExit(self, other)
-	if other:CompareTag("Player") then
-		playerInside = false
-	end
+    if other:CompareTag("Player") then
+        self.playerInside = false
+        Engine.Log("[SceneLoader_lvl2] Jugador salio del trigger.")
+    end
 end
-
-
-
-
-
